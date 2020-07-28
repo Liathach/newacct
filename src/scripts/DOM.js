6d43b4525ae936863371e3423ad3ae356ec1f158
David Robertson <drobertson2252@gmail.com>
2020-07-27 21:03:38 -0600

    First commit

---
+++ src/scripts/DOM.js
@@ -0,0 +1,70 @@
+const DOMfunctions = {
+
+    buildNewCard(node, text) {
+
+        // const cardSection = document.getElementById("idAcctCardSection");
+
+        const div = document.createElement('div');
+
+        div.setAttribute('class', 'clAcctCards');
+        div.setAttribute('id', text.name);
+        div.appendChild(document.createTextNode(text.name));
+
+        let cardTitle = document.createElement("strong");
+        cardTitle.setAttribute('id', `cardTitle ${text.name}`);
+        div.appendChild(cardTitle);
+
+        cardTitle.classList.add("text-center");
+        div.appendChild(cardTitle);
+
+        const breakLine1 = document.createElement("br");
+        div.appendChild(breakLine1);
+
+        const labelCurrBal = document.createElement("strong");
+        labelCurrBal.textContent = "Current Balance:";
+        labelCurrBal.classList.add("text-center");
+        div.appendChild(labelCurrBal);
+
+        const breakLine2 = document.createElement("br");
+        div.appendChild(breakLine2);
+
+        let currentBalance = document.createElement("P");
+        currentBalance.id = `idAcctCard ${text.name}`;
+        currentBalance.textContent = "$" + text.balance;
+        currentBalance.classList.add("text-center");
+        div.appendChild(currentBalance);
+
+        const removeAcctButton = document.createElement('button');
+        removeAcctButton.append(document.createTextNode("Remove Account"));
+        removeAcctButton.classList.add("button");
+        div.appendChild(removeAcctButton);
+
+        const breakLine3 = document.createElement("br");
+        div.appendChild(breakLine3);
+
+        node.parentElement.insertBefore(div, node);
+
+        return div;
+
+    },
+
+    citiesDropDownList(text) {
+        const dropdownList = document.createElement('OPTION');
+        dropdownList.setAttribute('id', text.name);
+        dropdownList.appendChild(document.createTextNode(text.name));
+        return dropdownList;
+    },
+
+    attachDropDown(node, text) {
+        const dropdownList = DOMfunctions.citiesDropDownList(text);
+        console.log(node);
+        node.appendChild(dropdownList, node);
+    },
+
+    deleteAcctCard(node) {
+        node.remove();
+    },
+
+};
+
+export default DOMfunctions;
---
+++ src/scripts/index.js
@@ -0,0 +1,98 @@
+import DOMfunctions from './DOM.js';
+import ooStuff from './account.js';
+
+let AcctControls = new ooStuff.AcctControls();
+let i = 0;
+
+document.body.addEventListener("click", (e) => {
+
+    if (e.target.nodeName === 'BUTTON') {
+
+        if (e.target.textContent === 'Add Account') {
+
+            const cardSection = document.getElementById("idAcctCards");
+
+            let div = document.createElement("div");
+
+            cardSection.appendChild(div);
+
+            if (idAcctName.value.length > 0 && idInitDeposit.value.length > 0) {
+                AcctControls.addAcct(idAcctName.value, Number(idInitDeposit.value));
+                DOMfunctions.buildNewCard(idAcctCards, (AcctControls.acctArray[i]));
+                DOMfunctions.attachDropDown(idDropDown, (AcctControls.acctArray[i]));
+                i++;
+                idAccountStatusMsg.innerText = `The total balance of all accounts is $${AcctControls.totalAcctBalance()}`;
+                clearFields();
+            }
+        } else if (e.target.textContent === "Deposit") {
+            if (lengthCheck() > 0) {
+                AcctControls.depositToAcct(idDropDown.value, Number(idAmount.value).toFixed(2));
+                updateAccounts();
+                idAccountStatusMsg.innerText = `The total balance of all accounts is $${AcctControls.totalAcctBalance()}`;
+                clearFields();
+            }
+        } else if (e.target.textContent === "Withdraw") {
+            if (lengthCheck() > 0) {
+                AcctControls.withdrawFromAcct(idDropDown.value, Number(idAmount.value).toFixed(2));
+                updateAccounts();
+                idAccountStatusMsg.innerText = `The total balance of all accounts is $${AcctControls.totalAcctBalance()}`;
+                clearFields();
+            }
+        } else if (e.target.textContent === "Remove Account") {
+            AcctControls.removeAcct(event.target.parentElement.id);
+            idAccountStatusMsg.innerText = `The total balance of all accounts is $${AcctControls.totalAcctBalance()}`;
+            DOMfunctions.deleteAcctCard(event.target.parentElement);
+
+            let dropDown = event.target.parentElement.id;
+
+            let counter = 0;
+            for (let n = 0; n < idDropDown.length; n++) {
+
+                if (dropDown === idDropDown[n].id) {
+                    counter = n;
+                }
+
+            };
+            idDropDown.removeChild(idDropDown[counter]);
+            return i--;
+
+        } else if (e.target.textContent === "Lowest Account Balance") {
+
+            idAccountStatusMsg.innerText = `The lowest value account and balance is: ${AcctControls.lowestAcctBalance()}`;
+        
+        } else if (e.target.textContent === "Highest Account Balance") {
+
+            idAccountStatusMsg.innerText = `The highest value account and balance is: ${AcctControls.highestAcctBalance()}`;
+
+    } else if (e.target.textContent === "Total Account Balance") {
+
+        idAccountStatusMsg.innerText = `The total balance of all accounts is: ${AcctControls.totalAcctBalance()}`;
+    }
+    }
+
+function clearFields() {
+    idAcctName.value = "";
+    idInitDeposit.value = "";
+    idAmount.value = "";
+    idDropDown.value = "Select";
+};
+
+function lengthCheck() {
+    return idAmount.value.length;
+};
+
+function updateAccounts() {
+    let index = AcctControls.acctArray.findIndex(acctFinder => acctFinder.name === idDropDown.value);
+    let updateBalance = AcctControls.acctArray[index].name;
+    let counter = 0;
+    for (let n = 0; n < idDropDown.length; n++) {
+
+        if (updateBalance === idDropDown[n].id) {
+            counter = n;
+        }
+
+    };
+    if (updateBalance === idDropDown[counter].value) {
+        document.getElementById(`idAcctCard ${updateBalance}`).textContent = '$' + AcctControls.acctArray[index].balance;
+    }
+}})
